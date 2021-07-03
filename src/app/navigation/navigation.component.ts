98fe7fa023c384a13d87cc317e376482884de9ee
ahpatel128 <ahpatel128@hotmail.com>
2021-07-03 23:52:19 +0530

    Navigation Change

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent implements OnInit {
 
+export class NavigationComponent implements OnInit {
+  isActive: boolean = true;
+  formBuilder: any;
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+  }
+  changeNavigation(): void {
+    if (this.isActive === true) {
+      this.isActive = false;
+    }
+    else {
+      this.isActive = true;
+    }
+  }
 }
