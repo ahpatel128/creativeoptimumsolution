b127bbca825199ce0384574e89f25b98dce292c9
Akhil Patel <ahpatel128@hotmail.com>
2021-07-05 19:36:28 +1000

    new componants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,13 @@ import { ContactComponent } from './contact/contact.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { FooterComponent } from './footer/footer.component';
 import { APP_BASE_HREF } from '@angular/common';
+import { PageServicesComponent } from './page-services/page-services.component';
+import { PageAboutComponent } from './page-about/page-about.component';
+import { PageProductsComponent } from './page-products/page-products.component';
+import { PagePricingComponent } from './page-pricing/page-pricing.component';
+import { PageContactComponent } from './page-contact/page-contact.component';
+import { PageHomeComponent } from './page-home/page-home.component';
+
 
 @NgModule({
   declarations: [
@@ -21,7 +28,13 @@ import { APP_BASE_HREF } from '@angular/common';
     PortfolioComponent,
     ContactComponent,
     NavigationComponent,
-    FooterComponent
+    FooterComponent,
+    PageServicesComponent,
+    PageAboutComponent,
+    PageProductsComponent,
+    PagePricingComponent,
+    PageContactComponent,
+    PageHomeComponent
   ],
   imports: [
     BrowserModule,
